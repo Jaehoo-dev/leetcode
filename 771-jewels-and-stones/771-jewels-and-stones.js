35af18b3bf093509a4170324479f78c918e1d39a
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2022-01-11 05:33:12 +0900

    Time: 104 ms (20.90%), Space: 40.2 MB (56.18%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.js
+++ 771-jewels-and-stones/771-jewels-and-stones.js
@@ -5,23 +5,14 @@
  */
 const numJewelsInStones = function (jewels, stones) {
     const jewelsMap = getJewelsMap(jewels);
-    let result = 0;
     
-    for (const stone of stones) {
-        if (jewelsMap[stone]) {
-            result++;
-        }
-    }
-    
-    return result;
+    return [...stones].reduce((acc, stone) => jewelsMap[stone] ? ++acc : acc, 0);
 };
 
 const getJewelsMap = function (jewels) {
     const result = {};
     
-    for (const jewel of jewels) {
-        result[jewel] = true;
-    }
+    [...jewels].forEach(jewel => result[jewel] = true);
     
     return result;
 }
