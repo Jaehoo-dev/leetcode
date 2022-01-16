7f463bac46a339700d9199f39dc4fd696ccb39d9
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2022-01-16 15:12:22 +0900

    Time: 126 ms (11.38%), Space: 39.8 MB (86.99%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.js
+++ 419-battleships-in-a-board/419-battleships-in-a-board.js
@@ -8,9 +8,9 @@ function countBattleships(board) {
     board.forEach((row, i) => {
         row.forEach((cell, j) => {
             const isEmpty = board[i][j] === '.';
-            const isNotEndOfShip = board[i][j + 1] === 'X' || board[i + 1]?.[j] === 'X';
+            const isEndOfShip = board[i][j + 1] !== 'X' && board[i + 1]?.[j] !== 'X';
             
-            if (isEmpty || isNotEndOfShip) return;
+            if (isEmpty || !isEndOfShip) return;
             
             result++;
         });
